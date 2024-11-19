85ffab2fe3b58c5ff538bcd503461a3dc5758d3e
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2024-11-19 14:58:22 +0100

    Initial commit

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controllers;
+
+use App\View;
+
+class HomeController
+{
+    public function index(): View
+    {
+        return View::make('index');
+    }
+
+    public function uploadCSV(): View
+    {
+        return View::make('uploadCSV.php');
+    }
+}
