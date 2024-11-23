bd6c237a1b99c7182584418327ff1f0dbe9a8e2d
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2024-11-23 15:28:59 +0100

    Router with Attributes

--- app/Attributes/Route.php
+++ app/Attributes/Route.php
@@ -6,7 +6,7 @@ namespace App\Attributes;
 
 use Attribute;
 
-#[Attribute(Attribute::IS_REPEATABLE)]
+#[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_METHOD)]
 class Route
 {
     public function __construct(public string $routePath, public string $method = 'get')
